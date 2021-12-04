a55d26e1277442687ef01e64ece388f13b191aae
Zhicheng WANG <zcwang@thoughtworks.com>
2021-12-04 15:23:33 +0800

    feat(consumer-ui): 配置 graphql client

--- projects/consumer-ui/src/app/app.module.ts
+++ projects/consumer-ui/src/app/app.module.ts
@@ -1,11 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
 import { CommonNonBizModule } from 'common-non-biz';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GlobalLayoutComponent } from './layout/global-layout/global-layout.component';
 import { LayoutSharedModule } from './layout/layout-shared.module';
+
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  return {
+    link: httpLink.create({ uri: '/graphql' }),
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'no-cache',
+      },
+      query: {
+        fetchPolicy: 'no-cache',
+      },
+    },
+  };
+}
 
 @NgModule({
   declarations: [
@@ -14,11 +33,14 @@ import { LayoutSharedModule } from './layout/layout-shared.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     LayoutSharedModule,
     CommonNonBizModule,
   ],
-  providers: [],
+  providers: [
+    { provide: APOLLO_OPTIONS, useFactory: createApollo, deps: [HttpLink] },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
---
+++ proxy-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  '/graphql': {
+    target: 'http://localhost:28081',
+  },
+};
